fd75b945f1b34b3c1dd1677978634cd55bc6ecdd
Eduardo Vega <vega.eduardo5051gmail.com>
2026-07-17 18:27:10 -0500

    FE-05 crear entidades de habitaciones camas servicios y tarifas

---
+++ src/domain/entities/room.entity.ts
@@ -0,0 +1,13 @@
+export interface Room {
+  id: number
+  numero: string
+  piso: number
+  estado: string
+  descripcion: string
+  es_fumador: boolean
+  observaciones: string
+  created_at: string
+  updated_at: string
+  hotel: number
+  tipo_habitacion: number
+}
